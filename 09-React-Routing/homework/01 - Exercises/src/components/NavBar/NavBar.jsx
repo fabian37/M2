75c75f9f41a365f5ee8ef4c80ec7172c0219a48b
Fabian Gomez <fabian37gomez@gmail.com>
2022-11-04 17:35:10 -0500

    Updated homeworks

--- 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
+++ 09-React-Routing/homework/01 - Exercises/src/components/NavBar/NavBar.jsx
@@ -1,28 +1,35 @@
-import React from "react";
-import logo from "../../assets/logo.png";
-import logoHenry from "../../assets/logo-henry.png";
-import styleNav from "./NavBar.module.css";
+import React from 'react';
+import logo from '../../assets/logo.png';
+import logoHenry from '../../assets/logo-henry.png';
+import styleNav from './NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
-  return (
-    <div className={styleNav.container}>
-      <ul className={styleNav.menu}>
-        <li>
-          <img src={logoHenry} alt="logo-henry" />
-          <img src={logo} alt="logo" />
-        </li>
-        <li>
-          <h1>Central de Cruceros</h1>
-        </li>
-        <div className={styleNav.options}>
-          <li>
-            <span>Navieras</span>
-          </li>
-          <li>
-            <span>Promociones</span>
-          </li>
-        </div>
-      </ul>
-    </div>
-  );
+	return (
+		<div className={styleNav.container}>
+			<ul className={styleNav.menu}>
+				<li>
+					<NavLink to={'/'}>
+						<img src={logoHenry} alt="logo-henry" />
+						<img src={logo} alt="logo" />
+					</NavLink>
+				</li>
+				<li>
+					<h1>Central de Cruceros</h1>
+				</li>
+				<div className={styleNav.options}>
+					<li>
+						<NavLink to={'/shipping'}>
+							<span>Navieras</span>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to={'/promotions'}>
+							<span>Promociones</span>
+						</NavLink>
+					</li>
+				</div>
+			</ul>
+		</div>
+	);
 }
