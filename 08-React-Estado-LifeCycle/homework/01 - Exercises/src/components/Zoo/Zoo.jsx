724eba47aeaf28f13331d1289b963e854168ec34
Fabian Gomez <fabian37gomez@gmail.com>
2022-11-04 12:03:59 -0500

    test

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -6,10 +6,47 @@ import Species from '../Species/Species';
 import './Zoo.module.css';
 
 export default function Zoo() {
-   /* Escribe acá tu código */
-   return (
-      <div>
-         <h1>-</h1>
-      </div>
-   );
+	/* Escribe acá tu código */
+	const [zoo, setZoo] = React.useState({
+		zooName: '',
+		animals: [],
+		species: [],
+		allAnimals: [],
+	});
+
+	function handleInputChange(e) {
+		setZoo({ ...zoo, zooName: e.target.value });
+	}
+
+	function handleSpecies(e) {}
+
+	React.useEffect(() => {
+		fetch('http://localhost:3001/zoo')
+			.then((res) => res.json())
+			.then((data) =>
+				setZoo({
+					...zoo,
+					animals: data.animals,
+					species: data.species,
+					allAnimals: data.animals,
+				})
+			)
+			.catch((error) => console.log(error));
+	}, []);
+
+	return (
+		<div>
+			<label htmlFor="nameInput">Zoo Name:</label>
+			<input
+				type="text"
+				name="nameInput"
+				value={zoo.zooName}
+				onChange={handleInputChange}
+			/>
+			<h1>{zoo.zooName}</h1>
+
+			<Species species={zoo.species} />
+			<Animals />
+		</div>
+	);
 }
